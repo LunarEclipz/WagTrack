2dad950ee750c32006ff02c8be853c2f2f563ea1
Zyon Wee <zyonwee@gmail.com>
2024-06-01 01:42:03 +0800

    feat : dropdown options

---
+++ lib/shared/dropdown_options.dart
@@ -0,0 +1,28 @@
+// For Milestone 1, our location will be the following 5. In future, we
+// may transit to using postal codes.
+
+List<String> locationList = <String>[
+  "North",
+  "North East",
+  "South",
+  "West",
+  "Central"
+];
+
+List<String> sexList = <String>[
+  "Female",
+  "Female Spayed",
+  "Male",
+  "Male Neutered",
+];
+
+List<String> speciesList = <String>[
+  "Cat",
+  "Dog",
+];
+
+List<String> rolesList = <String>[
+  "Owner",
+  "Co-Owner",
+  "Caretaker",
+];
