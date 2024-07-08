ddb7dc8f9c378813537291fd04314d7c3a7d34f0
Zeepheru <59129423+Zeepheru@users.noreply.github.com>
2024-07-09 03:09:45 +0800

    feat: add WIP page as placeholder

--- lib/screens/misc_pages.dart
+++ lib/screens/misc_pages.dart
@@ -23,6 +23,33 @@ class BlankPage extends StatelessWidget {
   }
 }
 
+/// 'Work in Progress' page. Used as a placeholder for when "stuff" is supposed to be here :)
+///
+/// Comes with the standard white-on-red appBar
+class WorkInProgressPage extends StatelessWidget {
+  const WorkInProgressPage({super.key});
+
+  // TODO WIP page needs a a funky image or some shit
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'WIP',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: colorScheme.primary,
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
 /// Full-screen loading page.
 class LoadingPage extends StatelessWidget {
   const LoadingPage({super.key});
