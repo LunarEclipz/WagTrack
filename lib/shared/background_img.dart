c03d7a70d9e04e4de8f71b27f5da44b159c0624a
Zeepheru <59129423+Zeepheru@users.noreply.github.com>
2024-07-11 04:21:29 +0800

    style: revert change to background image because it broke other things :(

--- lib/shared/background_img.dart
+++ lib/shared/background_img.dart
@@ -10,24 +10,38 @@ class BackgroundImageWrapper extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        child: Container(
-          height: screenHeight,
-          width: screenWidth,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              // Needs to be an SVG for higher definition
-              image: AssetImage('assets/background.png'),
-              opacity: 0.25,
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: child,
+    // return Scaffold(
+    //   resizeToAvoidBottomInset: false,
+    //   body: SingleChildScrollView(
+    //     physics: const NeverScrollableScrollPhysics(),
+    //     child: Container(
+    //       height: screenHeight,
+    //       width: screenWidth,
+    //       decoration: const BoxDecoration(
+    //         image: DecorationImage(
+    //           // Needs to be an SVG for higher definition
+    //           image: AssetImage('assets/background.png'),
+    //           opacity: 0.25,
+    //           fit: BoxFit.cover,
+    //         ),
+    //       ),
+    //       child: child,
+    //     ),
+    //   ),
+    // );
+
+    return Container(
+      height: screenHeight,
+      width: screenWidth,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          // Needs to be an SVG for higher definition
+          image: AssetImage('assets/background.png'),
+          opacity: 0.25,
+          fit: BoxFit.cover,
         ),
       ),
+      child: child,
     );
   }
 }
