9b28fb49c129d8a0ed1c53d602e3c95c309b321a
Zeepheru <59129423+Zeepheru@users.noreply.github.com>
2024-07-11 03:47:43 +0800

    fix: stopped background image from resizing when the onscreen keyboard appears
    source: https://stackoverflow.com/questions/46551268/when-the-keyboard-appears-the-flutter-widgets-resize-how-to-prevent-this

--- lib/shared/background_img.dart
+++ lib/shared/background_img.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class BackgroundImageWrapper extends StatelessWidget {
   final Widget child;
@@ -10,18 +10,24 @@ class BackgroundImageWrapper extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.height;
 
-    return Container(
-      height: screenHeight,
-      width: screenWidth,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          // Needs to be an SVG for higher definition
-          image: AssetImage('assets/background.png'),
-          opacity: 0.25,
-          fit: BoxFit.cover,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Container(
+          height: screenHeight,
+          width: screenWidth,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              // Needs to be an SVG for higher definition
+              image: AssetImage('assets/background.png'),
+              opacity: 0.25,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: child,
         ),
       ),
-      child: child,
     );
   }
 }
