2c44969716b89c58f94154ee8238f1d85a5ee43f
Zyon Wee <zyonwee@gmail.com>
2024-07-17 16:59:19 +0800

    feat : added pet post filter

--- lib/screens/posts/pet_posts_page.dart
+++ lib/screens/posts/pet_posts_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wagtrack/models/pet_model.dart';
+import 'package:wagtrack/services/pet_service.dart';
+import 'package:wagtrack/shared/dropdown_options.dart';
+import 'package:wagtrack/shared/themes.dart';
 
 class PetPostsPage extends StatefulWidget {
   final Pet petData;
@@ -11,14 +16,53 @@ class PetPostsPage extends StatefulWidget {
 }
 
 class _PetPostsPageState extends State<PetPostsPage> {
+  late String filterSelected = "All";
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-        height: 300,
-        child: Center(
-            child: Text(
-          "WIP for Milestone 3",
-          style: TextStyle(fontSize: 30),
-        )));
+    Pet petData = widget.petData;
+
+    final CustomColors customColors = AppTheme.customColors;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final TextTheme textStyles = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(postCategory.length + 1, (index) {
+              return index == 0
+                  ? InkWell(
+                      onTap: () {
+                        setState(() {
+                          filterSelected = "All";
+                        });
+                      },
+                      child: Text(
+                        "All",
+                        style: filterSelected == "All"
+                            ? textStyles.bodyLarge
+                            : textStyles.bodyMedium,
+                      ))
+                  : InkWell(
+                      onTap: () {
+                        setState(() {
+                          filterSelected = postCategory[index - 1];
+                        });
+                      },
+                      child: Text(
+                        postCategory[index - 1],
+                        style: filterSelected == postCategory[index - 1]
+                            ? textStyles.bodyLarge
+                            : textStyles.bodyMedium,
+                      ));
+            }),
+          )
+        ],
+      ),
+    );
   }
 }
